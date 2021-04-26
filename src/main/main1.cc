80963c3aaee1825bb01e50e029b1c869d1b00332
LE GARREC Vincent <github@le-garrec.fr>
2021-04-26 20:25:16 +0200

    Decorator in main

--- src/main/main1.cc
+++ src/main/main1.cc
@@ -5,21 +5,28 @@
 #include <jessica/data/geotechnical/foundation_strip_impl.h>
 #include <jessica/data/load/vertical_eccentric.h>
 #include <jessica/data/load/vertical_eccentric_impl.h>
+#include <jessica/util/decorator/log_call.h>
+#include <jessica/util/decorator/log_duration.h>
 
 int main()
 {
   const auto load = std::make_shared<Jessica::Data::Load::VerticalEccentric<
-      Jessica::Data::Load::VerticalEccentricImpl>>()
+      Jessica::Util::Decorator::LogCall<Jessica::Util::Decorator::LogDuration<
+          Jessica::Data::Load::VerticalEccentricImpl>>>>()
                         ->E(0.2)
                         ->V(100.);
   const auto foundation =
       std::make_shared<Jessica::Data::Geotechnical::FoundationStrip<
-          Jessica::Data::Geotechnical::FoundationStripImpl>>()
+          Jessica::Util::Decorator::LogCall<
+              Jessica::Util::Decorator::LogDuration<
+                  Jessica::Data::Geotechnical::FoundationStripImpl>>>>()
           ->B(1.);
   const auto calc =
       std::make_shared<Jessica::Calc::Geotechnical::MeyerhofShallowFoundation<
-          Jessica::Calc::Geotechnical::MeyerhofShallowFoundationImpl>>(
-          load, foundation);
+          Jessica::Util::Decorator::LogCall<
+              Jessica::Util::Decorator::LogDuration<
+                  Jessica::Calc::Geotechnical::
+                      MeyerhofShallowFoundationImpl>>>>(load, foundation);
 
   std::cout << calc->Qref() << " and " << calc->B_() << "\n";
 }
