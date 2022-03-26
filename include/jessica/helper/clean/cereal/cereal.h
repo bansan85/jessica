27edd1841ce7577e9889a370e20825554b95ec68
LE GARREC Vincent <github@le-garrec.fr>
2022-03-26 08:50:04 +0100

    Update to latest emscripten and package.json

--- include/jessica/helper/clean/cereal/cereal.h
+++ include/jessica/helper/clean/cereal/cereal.h
@@ -4,6 +4,7 @@
   #pragma clang diagnostic push
   #pragma clang diagnostic ignored "-Wexit-time-destructors"
   #pragma clang diagnostic ignored "-Wzero-as-null-pointer-constant"
+  #pragma clang diagnostic ignored "-Wdeprecated-this-capture"
 #endif
 
 #include <cereal/cereal.hpp>
