1392bdf910af1603495ce14a423918f436bc5e80
LE GARREC Vincent <github@le-garrec.fr>
2022-07-04 21:45:48 +0200

    Fix leak
    +fix windows build

--- include/jessica/helper/clean/drogon/drogon.h
+++ include/jessica/helper/clean/drogon/drogon.h
@@ -28,6 +28,7 @@
 #ifdef _MSC_VER
   #pragma warning(push)
   #pragma warning(disable : 4242)
+  #pragma warning(disable : 4244)
   #pragma warning(disable : 4365)
   #pragma warning(disable : 4435)
   #pragma warning(disable : 4866)
